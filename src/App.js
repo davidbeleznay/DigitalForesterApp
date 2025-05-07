e14e76893cfc02d1fc42a54d80cf5de98baa9184
davidbeleznay <dbeleznay09@gmail.com>
2025-05-07 15:05:29 -0700

    Add initial React components and setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Dashboard from './pages/Dashboard';
+import RoadRiskForm from './pages/RoadRiskForm';
+import CulvertSizingForm from './pages/CulvertSizingForm';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/culvert-sizing" element={<CulvertSizingForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
