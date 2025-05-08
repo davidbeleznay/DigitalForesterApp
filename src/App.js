95f117f07e23d733f0b34edeeb54aa6eda76f8a6
davidbeleznay <dbeleznay09@gmail.com>
2025-05-07 21:24:40 -0700

    feat: Add History page to application routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import RoadRiskForm from './pages/RoadRiskForm';
 import CulvertSizingForm from './pages/CulvertSizingForm';
+import HistoryPage from './pages/HistoryPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/road-risk" element={<RoadRiskForm />} />
         <Route path="/culvert-sizing" element={<CulvertSizingForm />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
   );
