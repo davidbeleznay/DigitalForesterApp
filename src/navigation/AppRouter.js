22478d4a66445881e982af506e984fcd82a62de8
davidbeleznay <dbeleznay09@gmail.com>
2025-05-13 13:57:58 -0700

    Update AppRouter with consistent routes for all tools

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -1,21 +1,16 @@
-// src/navigation/AppRouter.js
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
-import InputScreen from '../screens/culvert/InputScreen';
-import ResultScreen from '../screens/culvert/ResultScreen';
+import CulvertToolNavigator from './CulvertToolNavigator';
 import RoadRiskForm from '../pages/RoadRiskForm';
 import HistoryPage from '../pages/HistoryPage';
-import ThemeToggle from '../components/ThemeToggle';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/culvert" element={<InputScreen />} />
-        <Route path="/culvert/result" element={<ResultScreen />} />
+        <Route path="/culvert/*" element={<CulvertToolNavigator />} />
         <Route path="/road-risk" element={<RoadRiskForm />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
