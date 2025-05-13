30d908b067f7435f85ab0531dd511118478887a9
davidbeleznay <dbeleznay09@gmail.com>
2025-05-12 22:44:19 -0700

    refactor: simplify App.js to use new AppRouter
    - Replace direct routing with AppRouter component
    - Remove old page imports
    - Streamline application structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import RoadRiskForm from './pages/RoadRiskForm';
-import CulvertSizingForm from './pages/CulvertSizingForm';
-import HistoryPage from './pages/HistoryPage';
+import AppRouter from './navigation/AppRouter';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/road-risk" element={<RoadRiskForm />} />
-        <Route path="/culvert-sizing" element={<CulvertSizingForm />} />
-        <Route path="/history" element={<HistoryPage />} />
-      </Routes>
-    </Router>
-  );
+  return <AppRouter />;
 }
 
 export default App;
