b01fd7625085986916894437c071c3b3b66dbc22
davidbeleznay <dbeleznay09@gmail.com>
2025-05-13 06:26:59 -0700

    update: import ThemeToggle in AppRouter
    - Add ThemeToggle import to AppRouter
    - Ensure ThemeToggle is available throughout the application

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import InputScreen from '../screens/culvert/InputScreen';
 import ResultScreen from '../screens/culvert/ResultScreen';
+import ThemeToggle from '../components/ThemeToggle';
 
 const AppRouter = () => {
   return (
