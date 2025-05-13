897b365a3a490862a63032110accd0bcd21091b4
davidbeleznay <dbeleznay09@gmail.com>
2025-05-13 15:38:57 -0700

    Update App.js to include home.css stylesheet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppRouter from './navigation/AppRouter';
 import './styles/index.css';
 import './styles/culvert-form.css';
+import './styles/home.css';
 
 function App() {
   return (
