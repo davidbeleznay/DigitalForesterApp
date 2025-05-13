84e75c66ce43898c15b1884d669480f6d578a9fe
davidbeleznay <dbeleznay09@gmail.com>
2025-05-12 22:43:42 -0700

    fix: replace React Navigation with React Router
    - Create AppRouter component for web-based routing
    - Set up routes for Home, Culvert Input, and Culvert Result screens
    - Remove native-specific navigation code

---
+++ src/navigation/AppRouter.js
@@ -0,0 +1,21 @@
+// src/navigation/AppRouter.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeScreen from '../screens/HomeScreen';
+import InputScreen from '../screens/culvert/InputScreen';
+import ResultScreen from '../screens/culvert/ResultScreen';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/culvert" element={<InputScreen />} />
+        <Route path="/culvert/result" element={<ResultScreen />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
