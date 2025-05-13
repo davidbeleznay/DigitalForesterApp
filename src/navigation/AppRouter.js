d99764be9dfe5602c31e9f588e4588ee3f57030a
davidbeleznay <dbeleznay09@gmail.com>
2025-05-13 12:15:30 -0700

    Add routing for Road Risk Assessment tool

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import InputScreen from '../screens/culvert/InputScreen';
 import ResultScreen from '../screens/culvert/ResultScreen';
+import RoadRiskForm from '../pages/RoadRiskForm';
+import HistoryPage from '../pages/HistoryPage';
 import ThemeToggle from '../components/ThemeToggle';
 
 const AppRouter = () => {
@@ -14,6 +16,8 @@ const AppRouter = () => {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/culvert" element={<InputScreen />} />
         <Route path="/culvert/result" element={<ResultScreen />} />
+        <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
   );
