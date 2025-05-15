d3580c27e9e47082bef69fd075a5bc623232edf4
davidbeleznay <dbeleznay09@gmail.com>
2025-05-15 12:44:13 -0700

    Fix RoadRiskNavigator to use React Router instead of React Navigation

--- src/navigation/RoadRiskNavigator.js
+++ src/navigation/RoadRiskNavigator.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Routes, Route } from 'react-router-dom';
 import RoadRiskForm from '../pages/RoadRiskForm';
-import { colors } from '../constants/constants';
-
-const Stack = createStackNavigator();
 
 const RoadRiskNavigator = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="RoadRiskForm"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.primary,
-        },
-        headerTintColor: colors.white,
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen 
-        name="RoadRiskForm" 
-        component={RoadRiskForm} 
-        options={{ title: 'Road Risk Assessment' }}
-      />
-    </Stack.Navigator>
+    <div className="road-risk-navigator">
+      <Routes>
+        <Route path="/" element={<RoadRiskForm />} />
+      </Routes>
+    </div>
   );
 };
 
