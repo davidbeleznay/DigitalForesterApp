8f155acf12b0e387e910a13fd34e3144c6f38d45
davidbeleznay <dbeleznay09@gmail.com>
2025-05-15 13:02:52 -0700

    Delete RoadRiskNavigator.js to simplify navigation

--- src/navigation/RoadRiskNavigator.js
+++ src/navigation/RoadRiskNavigator.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import RoadRiskForm from '../pages/RoadRiskForm';
-
-const RoadRiskNavigator = () => {
-  return (
-    <div className="road-risk-navigator">
-      <Routes>
-        <Route path="/" element={<RoadRiskForm />} />
-      </Routes>
-    </div>
-  );
-};
-
-export default RoadRiskNavigator;
+// Removing this file completely
