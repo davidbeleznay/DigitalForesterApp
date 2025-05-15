18f17d7c9a0dedb398ebb248ed359b797c6e92ad
davidbeleznay <dbeleznay09@gmail.com>
2025-05-15 12:40:28 -0700

    Update AppRouter to properly handle RoadRisk routes

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import CulvertToolNavigator from './CulvertToolNavigator';
+import RoadRiskNavigator from './RoadRiskNavigator';
 import RoadRiskForm from '../pages/RoadRiskForm';
 import HistoryPage from '../pages/HistoryPage';
 
@@ -11,7 +12,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/culvert/*" element={<CulvertToolNavigator />} />
-        <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/road-risk/*" element={<RoadRiskForm />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
