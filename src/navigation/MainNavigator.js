8c57913fb45e2da5b02803089571efa1e4bb1e3d
davidbeleznay <dbeleznay09@gmail.com>
2025-05-15 13:03:31 -0700

    Update MainNavigator.js to use React Router instead of React Navigation

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,57 +1,22 @@
 // src/navigation/MainNavigator.js
 
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import CulvertToolNavigator from './CulvertToolNavigator';
-import RoadRiskNavigator from './RoadRiskNavigator';
+import RoadRiskForm from '../pages/RoadRiskForm';
 import HistoryPage from '../pages/HistoryPage';
-import { colors } from '../constants/constants';
-
-const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.primary,
-        },
-        headerTintColor: colors.white,
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen 
-        name="Home" 
-        component={HomeScreen}
-        options={{
-          title: 'Digital Forester',
-        }}
-      />
-      <Stack.Screen 
-        name="CulvertTool" 
-        component={CulvertToolNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen 
-        name="RoadRiskTool" 
-        component={RoadRiskNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen 
-        name="History" 
-        component={HistoryPage}
-        options={{
-          title: 'Assessment History',
-        }}
-      />
-    </Stack.Navigator>
+    <div className="main-navigator">
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/culvert/*" element={<CulvertToolNavigator />} />
+        <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </div>
   );
 };
 
