6bd945971f89567bf69f056613da049e8560bbea
davidbeleznay <dbeleznay09@gmail.com>
2025-05-20 15:41:28 -0700

    Simplify Road Risk routing by directing to EditScreen directly

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import CulvertToolNavigator from './CulvertToolNavigator';
-import RoadRiskNavigator from './RoadRiskNavigator';
+import EditScreen from '../screens/roadRisk/EditScreen';
 import HistoryPage from '../pages/HistoryPage';
 
 const AppRouter = () => {
@@ -11,7 +11,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/culvert/*" element={<CulvertToolNavigator />} />
-        <Route path="/road-risk/*" element={<RoadRiskNavigator />} />
+        <Route path="/road-risk" element={<EditScreen />} />
+        <Route path="/road-risk/edit/:id" element={<EditScreen />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
