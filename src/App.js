a1d4d089380ec856c9b9e225a954a52e8a4d758f
davidbeleznay <dbeleznay09@gmail.com>
2025-05-20 16:11:39 -0700

    Update App.js with additional CSS imports

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './styles/progress-steps.css';
 import './styles/info-components.css';
 import './styles/culvert-results.css';
 import './styles/RoadRiskForm.css';
-import './styles/form-elements.css'; // For better form styling
+import './styles/form-elements.css';
+import './styles/form-sections.css';
 
 function App() {
   return (
