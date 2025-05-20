cac52b7a3a6ddf3e0e8212ad0cf62731cdf010f2
davidbeleznay <dbeleznay09@gmail.com>
2025-05-20 15:25:34 -0700

    Add RoadRiskNavigator for improved routing

--- src/navigation/RoadRiskNavigator.js
+++ src/navigation/RoadRiskNavigator.js
@@ -1,1 +1,15 @@
-// Removing this file completely
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import RoadRiskInputScreen from '../screens/roadRisk/InputScreen';
+import RoadRiskEditScreen from '../screens/roadRisk/EditScreen';
+
+const RoadRiskNavigator = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<RoadRiskInputScreen />} />
+      <Route path="/edit/:id" element={<RoadRiskEditScreen />} />
+    </Routes>
+  );
+};
+
+export default RoadRiskNavigator;
