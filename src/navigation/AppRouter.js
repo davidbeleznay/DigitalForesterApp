612af7dff6169889f9970fd8f5e721e3e96531f7
davidbeleznay <dbeleznay09@gmail.com>
2025-05-22 08:47:04 -0700

    Update AppRouter to include Road Risk route

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
-import CulvertToolNavigator from './CulvertToolNavigator';
-import EditScreen from '../screens/roadRisk/EditScreen';
+import RoadRiskForm from '../pages/RoadRiskForm';
+import CulvertSizingForm from '../pages/CulvertSizingForm';
 import HistoryPage from '../pages/HistoryPage';
+import Dashboard from '../pages/Dashboard';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/culvert/*" element={<CulvertToolNavigator />} />
-        <Route path="/road-risk" element={<EditScreen />} />
-        <Route path="/road-risk/edit/:id" element={<EditScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/culvert" element={<CulvertSizingForm />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
