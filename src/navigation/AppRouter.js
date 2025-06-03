d7b8492ca3bc03282469c131000f15451b6ea652
davidbeleznay <dbeleznay09@gmail.com>
2025-06-03 07:22:36 -0700

    Switch to HashRouter for better subdirectory compatibility

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import RoadRiskForm from '../pages/RoadRiskForm';
 import CulvertSizingForm from '../pages/CulvertSizingForm';
@@ -8,7 +8,7 @@ import Dashboard from '../pages/Dashboard';
 
 const AppRouter = () => {
   return (
-    <Router basename="/DigitalForesterApp">
+    <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/dashboard" element={<Dashboard />} />
